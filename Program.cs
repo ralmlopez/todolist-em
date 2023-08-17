a9a0f89d40340f209fac789cc12d046fcc2f2003
Roberto Lopez <roberto@rhlopez.com>
2023-08-17 17:35:07 -0500

    Refactor

--- Program.cs
+++ Program.cs
@@ -17,17 +17,18 @@ static void RunProgram()
 static bool HandleChoice()
 {
     var quit = false;
-
     var choice = Console.ReadLine();
 
     switch (choice)
     {
         case "1":
-            CreateTask(Console.ReadLine() ?? string.Empty);
+            Console.Write("Enter task name: ");
+            AppendEvent(Console.ReadLine() ?? string.Empty, EventType.TaskCreated);
             break;
 
         case "2":
-            FinishTask(Console.ReadLine() ?? string.Empty);
+            Console.Write("Enter completed task name: ");
+            AppendEvent(Console.ReadLine() ?? string.Empty, EventType.TaskCompleted);
             break;
 
         case "3":
@@ -40,24 +41,10 @@ static bool HandleChoice()
     return quit;
 }
 
-static void CreateTask(string item)
+static void AppendEvent(string task, string eventType)
 {
-    var record = $"{item},{false.ToString()},{DateTime.Now.ToString()}";
-
-    using (StreamWriter outputFile = new StreamWriter(FileName, true))
-    {
-        outputFile.WriteLine(record);
-    }
-}
-
-static void FinishTask(string item)
-{
-    var record = $"{item},{true.ToString()},{DateTime.Now.ToString()}";
-
-    using (StreamWriter outputFile = new StreamWriter(FileName, true))
-    {
-        outputFile.WriteLine(record);
-    }
+    var record = $"{task},{eventType},{DateTime.Now}";
+    File.AppendAllLines(FileName, new[] { record });
 }
 
 static void ShowMenu()
@@ -69,67 +56,70 @@ static void ShowMenu()
     Console.Write("Enter choice: ");
 }
 
-
 static void ShowTasks()
 {
     const int spaces = 10;
     const int listWidth = 20;
     var blankSpaces = new string(' ', spaces);
     var listHorizontalBorder = new string('-', listWidth);
+    var headerFormat = $" {{0, -{listWidth}}}{blankSpaces}  {{1, -{listWidth}}}";
 
     Console.Clear();
-
-    Console.WriteLine($" {FormatForDisplay("NOT DONE")}{blankSpaces}  {FormatForDisplay("DONE")}");
+    Console.WriteLine(string.Format(headerFormat, "TASKS", "DONE"));
     Console.WriteLine($" {listHorizontalBorder}{blankSpaces}  {listHorizontalBorder}");
 
-    var todo = GetTaskListNotDone();
-    var completed = GetTaskListDone();
 
-    var joined = todo.Zip(completed, (done, notDone) => new { Done = done, NotDone = notDone })
-        .Concat(todo.Skip(completed.Count()).Select(done => new { Done = done, NotDone = string.Empty }))
-        .Concat(completed.Skip(todo.Count()).Select(notDone => new { Done = string.Empty, NotDone = notDone }))
-        .ToList();
-
-    foreach (var task in joined)
+    if (File.Exists(FileName))
     {
-        Console.WriteLine($"|{FormatForDisplay(task.Done)}|{new string(' ', spaces)}|{FormatForDisplay(task.NotDone)}|");
+        var todo = GetTasksPending().ToList();
+        var completed = GetTasksCompleted().ToList();
+
+        int maxCount = Math.Max(todo.Count, completed.Count);
+        for (int i = 0; i < maxCount; i++)
+        {
+            var taskDone = i < todo.Count ? todo[i] : string.Empty;
+            var taskNotDone = i < completed.Count ? completed[i] : string.Empty;
+            Console.WriteLine($"|{FormatForDisplay(taskDone)}|{blankSpaces}|{FormatForDisplay(taskNotDone)}|");
+        }
     }
 
     Console.WriteLine($" {listHorizontalBorder}{blankSpaces}  {listHorizontalBorder}");
     Console.WriteLine();
 }
 
-static string FormatForDisplay(string item)
+static string FormatForDisplay(string task)
 {
     const int maxLength = 20;
-    return item.Length > maxLength ? item.Substring(0, maxLength) : $"{item}{new string(' ', maxLength - item.Length)}";
+    return task.Length > maxLength ? task.Substring(0, maxLength) : task.PadRight(maxLength);
 }
 
-static IEnumerable<string> GetTaskListNotDone()
+static IEnumerable<string> GetTasksPending()
 {
     var allTasks = File
         .ReadLines(FileName)
         .Select(line => line.Split(','))
-        .Select(items => new { Task = items[0], Done = bool.Parse(items[1]) })
+        .Select(items => new { Task = items[0], EventType = items[1] })
         .ToList();
 
-    var allDone = allTasks
-        .Where(x => x.Done)
-        .Select(x => x.Task)
-        .ToList();
+    var allDone = new HashSet<string>(allTasks.Where(x => x.EventType == EventType.TaskCompleted).Select(x => x.Task));
 
     return allTasks
-        .Where(x => !x.Done && !allDone.Contains(x.Task))
-        .Select(x => x.Task)
-        .ToArray();
+        .Where(x => x.EventType == EventType.TaskCreated && !allDone.Contains(x.Task))
+        .Select(x => x.Task);
 }
 
-static IEnumerable<string> GetTaskListDone()
+static IEnumerable<string> GetTasksCompleted()
 {
     return File
         .ReadLines(FileName)
         .Select(line => line.Split(','))
-        .Where(items => items[1] == true.ToString())
+        .Where(items => items[1] == EventType.TaskCompleted)
         .Select(items => items[0])
         .ToList();
+}
+
+static class EventType
+{
+    public const string TaskCreated = "TaskCreated";
+    public const string TaskCompleted = "TaskCompleted";
 }
